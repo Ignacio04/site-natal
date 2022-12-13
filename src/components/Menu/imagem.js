22d866ddcb2aeab58a037c4ac0fc0c7fe8fd61a3
tomaee <tomastrindade@gmail.com>
2022-12-13 20:50:13 -0300

    mais arrumado

--- src/components/Menu/imagem.js
+++ src/components/Menu/imagem.js
@@ -5,9 +5,9 @@ import './imagem.css'
 export default function ImgNatal(){
     return(
         <div className="ImgExemplo">
-            <img src="legal1600.jpg" alt="Imagem das bolinha"/>
+            <img id="teste" src="legal1600.jpg" alt="Imagem das bolinha"/>
             <img src="legal1600inv.jpg" alt="Imagem das bolinha"/>
-            <figcaption>Promoçâo de Natal na DecorHouse!</figcaption>
+            <figcaption>Promoção de Natal na DecorHouse!</figcaption>
         </div>
     /*<figure id="container">
         <img src="natal-exemplo.jpg" alt="Imagem das bolinha" />  
