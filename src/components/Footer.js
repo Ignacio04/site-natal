d523c408fa65090cf6a123b2624b4b4f0790d967
tomaee <tomastrindade@gmail.com>
2022-12-16 23:11:12 -0300

    pronto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import './Footer.css';
 function Footer() {
     return (
       <div className="Footer">
-        <p></p>
+        <p className="popo">
+          <a className="sla" href="/">Termos de Uso</a>
+          <p>   |   </p>
+          <a className="sla" href="/">Política de Privacidade</a>
+          <p>   |   </p>
+          <a className="sla" href="/">Política de Cookies</a>
+          </p>
       </div>
     );
   }
